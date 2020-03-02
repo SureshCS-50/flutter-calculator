b1b1f29be4f90b4643b7067815e17ee3dbdd7604
Sureshkumar <sureshkumar.b@edpulsed.com>
2020-03-02 23:01:03 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/sureshcs50/calculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sureshcs50.calculator
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
